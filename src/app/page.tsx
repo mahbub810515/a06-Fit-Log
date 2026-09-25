725bd9d872e15098f7df60f06f1fe00449585886
mahbub810515 <mahbub810515@gmail.com>
2026-09-26 00:13:07 +0600

    library component added to the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Banner from '@/component/Banner'
+import Library from '@/component/library/Library'
 import React from 'react'
 
 const Page = () => {
   return (
     <div className='bg-slate-900'>
       <Banner/>
+      <Library/>
     </div>
   )
 }
---
+++ src/component/library/Library.tsx
@@ -0,0 +1,14 @@
+
+
+const Library = () => {
+  return (
+    <div className="container mx-auto">
+        <div>
+            <h1 className="font-extrabold text-2xl text-white">THE LIBRARY</h1>
+            <p className="font-normal text-[14px] text-gray-400">Twelve lifts covering every major muscle group.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Library

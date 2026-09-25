df6dfcaab3e596d8c99c5b6f1c6559a4143ef7a9
mahbub810515 <mahbub810515@gmail.com>
2026-09-26 01:14:14 +0600

    footer component added to main layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/component/NavBar";
+import Footer from "@/component/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full flex flex-col">
         <NavBar />
         {children}
+        <Footer/>
         </body>
     </html>
   );
